656edfa67867e0874ce357914adfd643699126dd
charlotter1277 <127cren@gmail.com>
2025-03-29 23:28:00 -0700

    Update 04_eda_viz.R to use sourced plotting functions

--- scripts/04_eda_viz.R
+++ scripts/04_eda_viz.R
@@ -6,37 +6,28 @@ the data set. The visualizations and tables will be saved as files.
 
 Usage: 04_eda_viz.R --input_unprocessed=<input_unprocessed> --input_processed=<input_processed> --output_fig1=<output_fig1> --output_fig2=<output_fig2>
 " -> doc
+
 library(tidyverse)
 library(docopt)
 library(GGally)
 
-# Source the create_pairs_plot function from root directory
-source("R/eda_viz_pairplot.R")
+# 🔧 Source required plotting functions
+source("R/eda_viz_pairplot.R")         # for create_pairs_plot()
+source("R/eda_viz.R")                  # for plot_anxiety_time_series()
 
+# Parse command line options
 opt <- docopt(doc)
 
-# read unprocessed and processed data
+# Read unprocessed and processed data
 unprocessed_data_path <- opt$input_unprocessed
 processed_data_path   <- opt$input_processed
 
 us_covid    <- read_csv(unprocessed_data_path)
 us_selected <- read_csv(processed_data_path)
 
-
-# Figure 1: Pair plot of all variables
+# 📊 Figure 1: Pair plot of all variables (from processed data)
 create_pairs_plot(us_selected, opt$output_fig1, width = 16, height = 12)
 
-# Figure 2: Time series plot on anxiety search trend across all COVID-19 dates
-
-options(repr.plot.width = 14, repr.plot.height = 9) 
-
-anxiety_time_plot <- ggplot(us_covid, aes(y = search_trends_anxiety, x = date)) +
-                      geom_line(color = "cornflower blue")+
-                      labs(title = "Google Searches Regarding Anxiety over Time",
-                           x = "Date",
-                           y = "Normalized Search Volume") +
-                        theme(text = element_text(size = 13)) 
-
-
-anxiety_time_plot
-ggsave(opt$output_fig2, width = 10)
+# 📈 Figure 2: Time series plot on anxiety search trend (from raw data)
+anxiety_time_plot <- plot_anxiety_time_series(us_covid)
+ggsave(opt$output_fig2, plot = anxiety_time_plot, width = 10)
